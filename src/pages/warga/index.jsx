fa4530e09fe11e78ac0d841e9599322002d2eb0c
syifaach <syifachrnnsas@gmail.com>
2025-07-06 22:04:34 +0700

    add condition to show hide form and list

--- src/pages/warga/index.jsx
+++ src/pages/warga/index.jsx
@@ -1,18 +1,30 @@
+import { useState } from "react";
+import FormAddKepalaKeluarga from "./components/FormAddKepalaKeluarga";
 import FormAddWarga from "./components/FormAddWarga";
 import ListKepalaKeluarga from "./components/ListKepalaKeluarga";
 import ListWarga from "./components/ListWarga";
 
 const Peoples = () => {
+  const [isAddPeoples, setIsAddPeoples] = useState(false);
+  const [isAddHeadFamily, setIsAddHeadFamily] = useState(false);
+
   return (
     <>
-      <div style={{ marginBottom: "30px" }}>
-        <FormAddWarga />
-        <ListWarga />
-      </div>
+      {isAddPeoples ? (
+        <FormAddWarga setIsAddPeoples={setIsAddPeoples} />
+      ) : isAddHeadFamily ? (
+        <FormAddKepalaKeluarga />
+      ) : (
+        <>
+          <div style={{ marginBottom: "30px" }}>
+            <ListWarga setIsAddPeoples={setIsAddPeoples} />
+          </div>
 
-      <div>
-        <ListKepalaKeluarga />
-      </div>
+          <div>
+            <ListKepalaKeluarga />
+          </div>
+        </>
+      )}
     </>
   );
 };
